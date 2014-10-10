80c4d2e31a8ecad8555865dc6cc38b2a5807e896
Todd Owen <todd.m.owen@gmail.com>
2014-10-11 07:19:10 +0800

    Write the logic to remove pairs of corresponding transactions.
    Noticed that this would have been easier if a Transaction did not
    include the balance, because we could do translist1.remove(...).

--- usaverultra.py
+++ usaverultra.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
 
 import sys
-from itertools import takewhile
+from itertools import takewhile, groupby
+from collections import namedtuple
 
+
+Transaction = namedtuple(
+	'Transaction',
+	['date', 'description', 'amount', 'balance']
+)
 
 class History(object):
 	def __init__(self, account_type, account_number, transactions):
@@ -62,17 +68,15 @@ def read_history(filename):
 
 		if len(row) != 5:
 			error('Transaction row with incorrect number of columns')
-		print row
 		_, s_transdate, description, s_amount, s_balance = row
 		transdate = datetime.strptime(s_transdate, '%d/%m/%Y').date()
 		amount = Decimal(s_amount.replace('$', '').replace(',', '')
 				.replace('--', ''))  # workaround for double negative
-		print s_balance
 		m = re.match(r'^\$([0-9,.]+) ([CD]R)$', s_balance)
 		balance = Decimal(m.group(1).replace(',', ''))
 		if m.group(2) == 'DR':
 			balance = -balance
-		return (transdate, description, amount, balance)
+		return Transaction(transdate, description, amount, balance)
 
 	import csv
 	with open(filename, 'rb') as csvfile:
@@ -83,6 +87,48 @@ def read_history(filename):
 	transactions = [get_transaction(row) for row in body
 			if not row[2].startswith('PLEASE NOTE')]
 	return History(account_type, account_number, transactions)
+
+
+def unify_histories(histories):
+	def remove_corresponding(acnum0, trans0, acnum1, translist1):
+		def fmt(s): return s.format(acnum0, acnum1)
+		prefix_pairs = [(fmt(x), fmt(y)) for x, y in [
+			('Sweep from {1}', 'Sweep out to {0}'),
+			('Sweep out to {1}', 'Sweep from {0}'),
+			('Funds Transfer to {1}', 'Funds Transfer'),
+			('Funds Transfer', 'Funds Transfer to {0}'),
+		]]
+
+		for prefix, co_prefix in prefix_pairs:
+			if trans0.description.startswith(prefix):
+				co_description = trans0.description.replace(
+						prefix, co_prefix)
+				co_amount = -trans0.amount
+				break
+		else:
+			return False
+
+		for i, trans1 in enumerate(translist1):
+			if trans1.date == trans0.date \
+					and trans1.description == co_description \
+					and trans1.amount == co_amount:
+				translist1.pop(i)
+				return True
+
+		return False
+
+	acnums = [h.account_number for h in histories]
+	if len(acnums) != 2 or acnums[0] == acnums[1]:
+		raise Exception('Requires exactly two different accounts. ' +
+				'Account numbers were: ' +
+				','.join(acnums))
+
+	a = list(histories[0].transactions)
+	b = [t for t in histories[1].transactions
+			if not remove_corresponding(acnums[1], t, acnums[0], a)]
+
+	#getdate = lambda trans: trans.date
+	#alltrans = sorted(chain(h.transactions for h in histories), key=getdate)
 
 
 if __name__ == '__main__':
